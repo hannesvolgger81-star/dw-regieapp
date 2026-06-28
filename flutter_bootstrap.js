da1a1711bbce85dfe305b4bd6c328491900df552
Hannes Volgger <hannesvolgger81-star@users.noreply.github.com>
2026-06-28 17:14:30 +0200

    Load Auftrag projects via public RPC

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "562129506" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4069697083" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
