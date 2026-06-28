8360cfa4f63fe8e81dd1ab19d8c2d5e4696433bb
Hannes Volgger <hannesvolgger81-star@users.noreply.github.com>
2026-06-28 16:17:46 +0200

    Add project creation form

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3970567648" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4230835373" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
