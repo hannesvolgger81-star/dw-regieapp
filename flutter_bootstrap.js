3c7b9503dbf65f1dda13772dd1caa811af2d640d
Hannes Volgger <hannesvolgger81-star@users.noreply.github.com>
2026-06-30 20:31:38 +0200

    Add expandable project location controls

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3234794620" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3525254861" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
