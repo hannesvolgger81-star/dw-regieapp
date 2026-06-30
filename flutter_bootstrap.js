02e82c5c16346663fd541ad0ae0d544a384bee04
Hannes Volgger <hannesvolgger81-star@users.noreply.github.com>
2026-06-30 21:28:30 +0200

    Use hosted satellite map picker

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4049725927" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "558317154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
